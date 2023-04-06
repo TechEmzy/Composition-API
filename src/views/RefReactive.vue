10684a5499a47d9511c9e119e74adc7a5942d7e8
unknown <emmanuelemafe@gmail.com>
2023-04-06 18:09:06 +0100

    second commit

--- src/views/RefReactive.vue
+++ src/views/RefReactive.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Ref vs Reactive</h1>
+  <div class="reactive">
+    <h1>Ref vs Reactive</h1>
+    <h3>using REACTIVE by importing it from the library</h3>
+    <br>
+
+    <!-- output the name and the age in the DOM -->
+    <p>{{ data.name }} - {{ data.age }}</p>
+
+    <!-- click button to update my name and my age -->
+    <button @click="updateData">udate my data</button>
+  </div>
+
 </template>
 
 <script>
-export default {
+import {ref, reactive} from vue
 
+export default {
+    // my component name
+    name: 'RefReactive',
+    // setup function
+    setup() {
+        const data = ref({ name: "Emmy", age: "29"})
+
+        // creating an UPDATE FUNCTION to change the age and name
+        const updateData = () => {
+            data.value.age = 35
+        }
+
+        return { data, updateData}
+    }
 }
 </script>
-
-<style>
-
-</style>
